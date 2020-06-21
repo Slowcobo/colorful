81d7e6188d7299cd6f07e3447e8084131a321e0f
Slowcobo <spencer.coleman92@gmail.com>
2020-06-21 14:54:13 -0700

    Adjust Palette route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedPalettes from "./seedPalettes";
 import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" render={() => <h1>Palette List Here</h1>} />
-        <Route
-          exact
-          path="/palette/:id"
-          render={() => <h1>Individual Palette Here</h1>}
-        />
-      </Switch>
-      <Palette palette={generatePalette(seedPalettes[4])} />
-    </div>
-  );
+class App extends Component {
+  findPalette(id) {
+    return seedPalettes.find((palette) => palette.id === id);
+  }
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Palette List Here</h1>} />
+          <Route
+            exact
+            path="/palette/:id"
+            render={(routeProps) => (
+              <Palette
+                palette={generatePalette(
+                  this.findPalette(routeProps.match.params.id)
+                )}
+              />
+            )}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
