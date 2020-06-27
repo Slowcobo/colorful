2580a37383429181dbd05d4c9db8b4591ccac224
Slowcobo <spencer.coleman92@gmail.com>
2020-06-26 18:49:42 -0700

    Move styles to new files

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -3,51 +3,8 @@ import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
 import PaletteFooter from "./PaletteFooter";
+import styles from "./styles/PaletteStyles";
 import { withStyles } from "@material-ui/styles";
-
-const styles = {
-  Palette: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    overflow: "hidden",
-  },
-  colors: {
-    height: "90%",
-  },
-  goBack: {
-    backgroundColor: "#000",
-    width: "20%",
-    height: "50%",
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-4px",
-    textTransform: "uppercase",
-    opacity: 1,
-    "& a": {
-      color: "#fff",
-      width: "100px",
-      height: "30px",
-      position: "absolute",
-      display: "inline-block",
-      top: "50%",
-      left: "50%",
-      marginLeft: "-50px",
-      marginTop: "-15px",
-      textAlign: "center",
-      outline: "none",
-      background: "rgba(255, 255, 255, 0.3)",
-      lineHeight: "30px",
-      border: "none",
-      fontSize: "0.9rem",
-      textTransform: "uppercase",
-      cursor: "pointer",
-      textDecoration: "none",
-    },
-  },
-};
 
 class SingleColorPalette extends Component {
   constructor(props) {
