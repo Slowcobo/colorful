3ad2fc03238d3f0def1e75ddc4d969f2e8d0cdc9
Slowcobo <spencer.coleman92@gmail.com>
2020-06-28 13:12:41 -0700

    Implement drag and drop

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -11,7 +11,8 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import Button from "@material-ui/core/Button";
-import DraggableColorBox from "./DraggableColorBox";
+import DraggableColorList from "./DraggableColorList";
+import { arrayMove } from "react-sortable-hoc";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { ChromePicker } from "react-color";
 
@@ -142,11 +143,17 @@ class NewPaletteForm extends Component {
     this.props.history.push("/");
   };
 
-  removeColor(colorName) {
+  removeColor = (colorName) => {
     this.setState({
       colors: this.state.colors.filter((color) => color.name !== colorName),
     });
-  }
+  };
+
+  onSortEnd = ({ oldIndex, newIndex }) => {
+    this.setState(({ colors }) => ({
+      colors: arrayMove(colors, oldIndex, newIndex),
+    }));
+  };
 
   render() {
     const { classes } = this.props;
@@ -248,14 +255,12 @@ class NewPaletteForm extends Component {
           })}
         >
           <div className={classes.drawerHeader} />
-          {this.state.colors.map((color) => (
-            <DraggableColorBox
-              key={color.name}
-              color={color.color}
-              name={color.name}
-              handleClick={() => this.removeColor(color.name)}
-            />
-          ))}
+          <DraggableColorList
+            colors={this.state.colors}
+            removeColor={this.removeColor}
+            axis="xy"
+            onSortEnd={this.onSortEnd}
+          />
         </main>
       </div>
     );
