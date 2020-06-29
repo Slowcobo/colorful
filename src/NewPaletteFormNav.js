88808306bfec3efce863857becab7b767ca61b74
Slowcobo <spencer.coleman92@gmail.com>
2020-06-28 22:59:42 -0700

    Apply initial NewPaletteFormNav style

--- src/NewPaletteFormNav.js
+++ src/NewPaletteFormNav.js
@@ -9,6 +9,37 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import { withStyles } from "@material-ui/core/styles";
+
+const drawerWidth = 400;
+
+const styles = (theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: "64px",
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20,
+  },
+  navBtns: {},
+});
 
 class NewPaletteFormNav extends Component {
   constructor(props) {
@@ -34,7 +65,7 @@ class NewPaletteFormNav extends Component {
     const { newPaletteName } = this.state;
 
     return (
-      <div>
+      <div className={classes.root}>
         <CssBaseline />
         <AppBar
           position="fixed"
@@ -53,8 +84,10 @@ class NewPaletteFormNav extends Component {
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" color="inherit" noWrap>
-              Persistent drawer
+              Create a Palette
             </Typography>
+          </Toolbar>
+          <div className={classes.navBtns}>
             <ValidatorForm
               onSubmit={() => this.props.handleSubmit(newPaletteName)}
             >
@@ -72,17 +105,17 @@ class NewPaletteFormNav extends Component {
               <Button type="submit" variant="contained" color="primary">
                 Save Palette
               </Button>
-              <Link to="/">
-                <Button variant="contained" color="secondary">
-                  Go Back
-                </Button>
-              </Link>
             </ValidatorForm>
-          </Toolbar>
+            <Link to="/">
+              <Button variant="contained" color="secondary">
+                Go Back
+              </Button>
+            </Link>
+          </div>
         </AppBar>
       </div>
     );
   }
 }
 
-export default NewPaletteFormNav;
+export default withStyles(styles, { withTheme: true })(NewPaletteFormNav);
