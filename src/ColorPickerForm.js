9446d7ced1150f2ca023ad72716ff148da9a3624
Slowcobo <spencer.coleman92@gmail.com>
2020-06-28 23:42:21 -0700

    Style ColorPickerForm

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { ChromePicker } from "react-color";
+import { withStyles } from "@material-ui/styles";
+
+const styles = {
+  picker: {
+    width: "100% !important",
+    marginTop: "2rem",
+  },
+  addColor: {
+    width: "100%",
+    padding: "0.5rem",
+    marginTop: "1rem",
+    fontSize: "1.5rem",
+  },
+  colorNameInput: {
+    width: "100%",
+    height: "60px",
+  },
+};
 
 class ColorPickerForm extends Component {
   constructor(props) {
@@ -48,19 +66,24 @@ class ColorPickerForm extends Component {
   }
 
   render() {
-    const { paletteIsFull } = this.props;
+    const { paletteIsFull, classes } = this.props;
     const { currentColor, newColorName } = this.state;
 
     return (
       <div>
         <ChromePicker
+          className={classes.picker}
           color={currentColor}
           onChangeComplete={this.updateCurrentColor}
         />
         <ValidatorForm onSubmit={this.handleSubmit}>
           <TextValidator
+            className={classes.colorNameInput}
+            variant="filled"
+            margin="normal"
             value={newColorName}
             name="newColorName"
+            placeholder="Color Name"
             onChange={this.handleChange}
             validators={["required", "colorNameUnique", "colorUnique"]}
             errorMessages={[
@@ -70,6 +93,7 @@ class ColorPickerForm extends Component {
             ]}
           />
           <Button
+            className={classes.addColor}
             variant="contained"
             color="primary"
             type="submit"
@@ -88,4 +112,4 @@ class ColorPickerForm extends Component {
   }
 }
 
-export default ColorPickerForm;
+export default withStyles(styles)(ColorPickerForm);
