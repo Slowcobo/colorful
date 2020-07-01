37b49da1efb799da112b9809ca045ce9313a90b9
Slowcobo <spencer.coleman92@gmail.com>
2020-07-01 13:01:25 -0700

    Refactor styles and add constants

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -2,39 +2,7 @@ import React from "react";
 import { SortableElement } from "react-sortable-hoc";
 import { withStyles } from "@material-ui/styles";
 import DeleteIcon from "@material-ui/icons/Delete";
-
-const styles = {
-  root: {
-    backgroundColor: (props) => props.color,
-    width: "20%",
-    height: "25%",
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-6px",
-    textTransform: "uppercase",
-    "&:hover svg": {
-      color: "#fff",
-      transform: "scale(1.5)",
-    },
-  },
-  boxContent: {
-    color: "rgba(0, 0, 0, 0.5)",
-    position: "absolute",
-    width: "100%",
-    left: "0",
-    bottom: "0",
-    padding: "10px",
-    letterSpacing: "1px",
-    fontSize: "12px",
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  deleteIcon: {
-    transition: "all 0.3s ease-in-out",
-  },
-};
+import styles from "./styles/DraggableColorBoxStyles";
 
 const DraggableColorBox = SortableElement((props) => {
   const { classes, handleClick, name } = props;
---
+++ src/styles/DraggableColorBoxStyles.js
@@ -0,0 +1,34 @@
+const styles = {
+  root: {
+    backgroundColor: (props) => props.color,
+    width: "20%",
+    height: "25%",
+    margin: "0 auto",
+    display: "inline-block",
+    position: "relative",
+    cursor: "pointer",
+    marginBottom: "-6px",
+    textTransform: "uppercase",
+    "&:hover svg": {
+      color: "#fff",
+      transform: "scale(1.5)",
+    },
+  },
+  boxContent: {
+    color: "rgba(0, 0, 0, 0.5)",
+    position: "absolute",
+    width: "100%",
+    left: "0",
+    bottom: "0",
+    padding: "10px",
+    letterSpacing: "1px",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  deleteIcon: {
+    transition: "all 0.3s ease-in-out",
+  },
+};
+
+export default styles;
